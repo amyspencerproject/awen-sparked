5eb80ec457dc6864c5c508c198c75774cb581232
amyspencerproject <amy@spencerproject.com>
2022-07-27 00:32:21 -0600

    Added a plugin for images but still no bakcground-image in CSS

--- .eleventy.js
+++ .eleventy.js
@@ -75,6 +75,28 @@ module.exports = function(eleventyConfig) {
     return content;
   });
 
+  /**** ASYNCHRONOUS SHORTCODE to get images into CSS for background ****/ 
+  const Image = require("@11ty/eleventy-img");
+
+  async function imageShortcode(src, alt) {
+    if(alt === undefined) {
+      // You bet we throw an error on missing alt (alt="" works okay)
+      throw new Error(`Missing \`alt\` on myImage from: ${src}`);
+    }
+  
+    let metadata = await Image(src, {
+      widths: [600],
+      formats: ["jpeg"]
+    });
+  
+    let data = metadata.jpeg[metadata.jpeg.length - 1];
+    return `<img src="${data.url}" width="${data.width}" height="${data.height}" alt="${alt}" loading="lazy" decoding="async">`;
+  }
+
+  eleventyConfig.addNunjucksAsyncShortcode("image", imageShortcode);
+  eleventyConfig.addLiquidShortcode("image", imageShortcode);
+  eleventyConfig.addJavaScriptFunction("image", imageShortcode);
+
   // Don't process folders with static assets e.g. images
   eleventyConfig.addPassthroughCopy("favicon.ico");
   eleventyConfig.addPassthroughCopy("static/img");
