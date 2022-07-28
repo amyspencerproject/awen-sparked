8898b812334b4038c578f1337e8d633c253d628a
amyspencerproject <amy@spencerproject.com>
2022-07-27 19:07:17 -0600

    Added a different image plugin

--- .eleventy.js
+++ .eleventy.js
@@ -75,27 +75,40 @@ module.exports = function(eleventyConfig) {
     return content;
   });
 
-  /**** ASYNCHRONOUS SHORTCODE to get images into CSS for background ****/ 
+  /* Asynch function from plugin install directions */
   const Image = require("@11ty/eleventy-img");
 
-  async function imageShortcode(src, alt) {
-    if(alt === undefined) {
-      // You bet we throw an error on missing alt (alt="" works okay)
-      throw new Error(`Missing \`alt\` on myImage from: ${src}`);
-    }
-  
-    let metadata = await Image(src, {
-      widths: [600],
-      formats: ["jpeg"]
+  (async () => {
+    let url = "https://images.unsplash.com/photo-1608178398319-48f814d0750c";
+    let stats = await Image(url, {
+      widths: [300]
     });
-  
-    let data = metadata.jpeg[metadata.jpeg.length - 1];
-    return `<img src="${data.url}" width="${data.width}" height="${data.height}" alt="${alt}" loading="lazy" decoding="async">`;
-  }
 
-  eleventyConfig.addNunjucksAsyncShortcode("image", imageShortcode);
-  eleventyConfig.addLiquidShortcode("image", imageShortcode);
-  eleventyConfig.addJavaScriptFunction("image", imageShortcode);
+    console.log( stats );
+  })();
+
+
+  /**** ASYNCHRONOUS SHORTCODE to get images into CSS for background ****/ 
+  // const Image = require("@11ty/eleventy-img");
+
+  // async function imageShortcode(src, alt) {
+  //   if(alt === undefined) {
+  //     // You bet we throw an error on missing alt (alt="" works okay)
+  //     throw new Error(`Missing \`alt\` on myImage from: ${src}`);
+  //   }
+  
+  //   let metadata = await Image(src, {
+  //     widths: [600],
+  //     formats: ["jpeg"]
+  //   });
+  
+  //   let data = metadata.jpeg[metadata.jpeg.length - 1];
+  //   return `<img src="${data.url}" width="${data.width}" height="${data.height}" alt="${alt}" loading="lazy" decoding="async">`;
+  // }
+
+  // eleventyConfig.addNunjucksAsyncShortcode("image", imageShortcode);
+  // eleventyConfig.addLiquidShortcode("image", imageShortcode);
+  // eleventyConfig.addJavaScriptFunction("image", imageShortcode);
 
   // Don't process folders with static assets e.g. images
   eleventyConfig.addPassthroughCopy("favicon.ico");
